5596dbeeba6212bce10919e31c7782d084fb83b0
QuachCongThinh <qcthinh3105@gmail.com>
2023-08-30 01:48:18 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
     strings: ["Front end Developer."],
-    typeSpeed: 100,
-    backSpeed: 100,
+    typeSpeed: 60,
+    backSpeed: 60,
     backDelay: 1000,
     loop: true
 });
