5e3f1822ffc3f5f3fcebf274aed3e1f84d924a31
QuachCongThinh <qcthinh3105@gmail.com>
2023-09-15 14:07:14 +0700

    UpdateCode

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+//Auto/Hidden mobile menu
+var btnMenu = document.getElementById('mobile-menu');
+var headerNav = document.querySelector('#header .navDiv');
+
+btnMenu.onclick = function(){
+    var isOpen = headerNav.style.transform === 'translate(0%)';
+    if (isOpen) {
+        
+    }
+}
+
+var menuItems = document.querySelectorAll('#header ul li a');
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    
+    menuItem.onclick = function(){
+        headerNav.style.transform = 'translateX(100%)';
+    }
+}
+
+//Display Text
 var typed = new Typed(".text", {
     strings: ["Front end Developer."],
     typeSpeed: 60,
